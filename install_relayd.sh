9355b97ad2ed1c78016152897ce9db6230c92b79
Alceu Rodrigues de Freitas Junior <arfreitas@cpan.org>
2019-11-10 17:42:02 -0300

    Small improvements

--- install_relayd.sh
+++ install_relayd.sh
@@ -9,8 +9,8 @@ echo 'eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)"' >> ~/.bashrc
 echo 'export PERL5LIB=$PERL5LIB:/usr/share/perl5' >> ~/.bashrc
 echo '. /home/runner/.bashrc' > ~/.bash_profile
 
-cpanm --local-lib=~/perl5 --notest POE::Component::SSLify
-cpanm --local-lib=~/perl5 --notest POE::Component::Client::HTTP
-cpanm --local-lib=~/perl5 metabase::relayd
+cpanm --local-lib=~/perl5 --notest --verbose POE::Component::SSLify
+cpanm --local-lib=~/perl5 --notest --verbose POE::Component::Client::HTTP
+cpanm --local-lib=~/perl5 --verbose metabase::relayd
 tar -cjf myperl.tar.bz2 .bashrc .bash_profile .cpan .cpanm/ perl5/
 chmod a+r myperl.tar.bz2
